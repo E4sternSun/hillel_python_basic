00e91d0877e56490740e7dc69d17b76a5276388a
EasternSun <terentiev.oleksii@gmail.com>
2025-09-05 22:59:52 +0300

    3.2

--- 3.2.py
+++ 3.2.py
@@ -1,7 +1,6 @@
-my_list = []
+list_calc = []
 
-if len(my_list) > 1:
-    last = my_list.pop()
-    my_list.insert(0, last)
+if len(list_calc) > 1:
+    list_calc.insert(0, list_calc.pop())
 
-print(my_list)
+print(list_calc)
