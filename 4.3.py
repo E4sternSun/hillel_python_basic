eec7da845bff63eb954c2d63be0af9ef3a3e11cb
EasternSun <terentiev.oleksii@gmail.com>
2025-09-10 19:35:33 +0300

    dz/4.3

--- 4.3.py
+++ 4.3.py
@@ -1,7 +1,7 @@
 import random
 
 lst = [random.randint(0, 9) for _ in range(random.randint(3, 10))]
-print("Исходный список:", lst)
+print("random list:", lst)
 
 new_lst = [lst[0], lst[2], lst[-2]]
-print("Новый список:", new_lst)
+print("new list:", new_lst)
