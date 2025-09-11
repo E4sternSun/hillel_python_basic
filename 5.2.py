b005eeca529fc71602dbb51d86576c54a052a3cf
EasternSun <terentiev.oleksii@gmail.com>
2025-09-11 15:39:11 +0300

    dz5.2.0

--- 5.2.py
+++ 5.2.py
@@ -1,18 +1,14 @@
 while True:
-    op = input("""Select operation (+, -, /, *) or "exit" for exit: """)
-    if op == "exit":
-        print("Bye!")
-        break
-
+    op = input("Select operation (+, -, /, *): ")
     a = int(input("Enter the first number for calculation: "))
     b = int(input("Enter the second number for calculation: "))
 
     if op == "+":
-        print(a + b)
+        print(f"{a} + {b} = {a + b}")
     elif op == "-":
-        print(a - b)
+        print(f"{a} - {b} = {a - b}")
     elif op == "*":
-        print(a * b)
+        print(f"{a} * {b} = {a * b}")
     elif op == "/":
         if b == 0:
             print("\n\033[91mYou can't divide by zero!\033[0m")
@@ -20,3 +16,7 @@ while True:
             print(a / b)
     else:
         print("\n\033[91mThe arithmetic operator was chosen incorrectly!\033[0m")
+    again = input("Do you want to calculate again? (yes/y to continue): ")
+    if again not in ["yes", "YES", "y", "Y"]:
+        print("Bye!")
+        break
