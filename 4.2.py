1f8f058257690b6421922fb7af291da7c67f9fdd
EasternSun <terentiev.oleksii@gmail.com>
2025-09-11 15:05:39 +0300

    dz4.2.0

--- 4.2.py
+++ 4.2.py
@@ -1,7 +1,7 @@
 value = []
 
 if value:
-    selected_indices = [0, 2, 4]
+    selected_indices = list(range(0, len(value), 2))
     selected_values = [value[x] for x in selected_indices if x < len(value)]
     result = sum(selected_values) * value[-1]
 else:
