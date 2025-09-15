0e8c7353037c97972ebe5a76facc6cb4c4f6e72b
EasternSun <terentiev.oleksii@gmail.com>
2025-09-15 17:25:13 +0300

    dz6.1

---
+++ 6.1.py
@@ -0,0 +1,17 @@
+import string
+
+while True:
+    user_input = input("Enter two letters separated by a hyphen : ")
+    start, end = user_input.split("-")
+
+    letters = string.ascii_letters
+    start_index = letters.index(start)
+    end_index = letters.index(end)
+
+    result = letters[start_index:end_index + 1]
+    print("Result:", result)
+
+    cont = input("Continue? (yes/y to proceed): ").lower()
+    if cont not in ["yes", "y"]:
+        print("See you later")
+        break
