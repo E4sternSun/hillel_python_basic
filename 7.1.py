efc1ff34931766b4b2a9e5b2d09a1dad068ae294
EasternSun <terentiev.oleksii@gmail.com>
2025-09-16 21:51:03 +0300

    dz7.1

---
+++ 7.1.py
@@ -0,0 +1,7 @@
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old"
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(say_hi(name, age))
