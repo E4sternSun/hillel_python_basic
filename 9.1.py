b0992fe52c5732af5616ad05bdc26d921fe4effc
EasternSun <terentiev.oleksii@gmail.com>
2025-09-26 18:01:20 +0300

    dz9.1.0

--- 9.1.py
+++ 9.1.py
@@ -8,7 +8,7 @@ def popular_words(text: str, words: list[str]) -> dict[str, int]:
     """
     text_words = text.lower().split()
     return {word: text_words.count(word) for word in words}
-pass
+
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
 ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
 print('OK')
